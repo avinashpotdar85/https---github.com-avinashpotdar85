0f548253f14eeb03fc9869e3b54e86d2190fc15f
avinashpotdar85 <avinashpotdar85@gmail.com>
2016-08-22 11:52:13 +0530

    Its a shopping cart backend code...
    add,delete,update,category,supplier,product methods are used...made use
    of maven tool...spring hibernate..

---
+++ ShopingCartNew/src/main/java/com/niit/shopingcart/dao/UserDetailsDAO.java
@@ -0,0 +1,27 @@
+package com.niit.shopingcart.dao;
+
+import java.util.List;
+
+import com.niit.shopingcart.model.UserDetails;
+import com.niit.shopingcart.model.shipping;
+
+public interface UserDetailsDAO {
+
+
+	public List<UserDetails> list();
+
+	public UserDetails get(String id);
+
+	public UserDetails getByName(String name);
+
+	public void saveOrUpdate(UserDetails userDetails);
+
+	public String delete(String id);
+
+	public void saveOrUpdate(shipping ship);
+
+	public boolean add(UserDetails userDetails);
+
+	public boolean delete(UserDetails userDetails);
+
+}
